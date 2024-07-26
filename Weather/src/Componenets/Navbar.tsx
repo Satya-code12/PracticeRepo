dbe8f5a4269916f8af7e621047095d60003e7da1
Satyabrata Ojha <satyabrat637@gmail.com>
2024-07-26 16:39:18 +0530

    Initial Commit

---
+++ Weather/src/Componenets/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () =>{
+  return(
+    <nav>
+      <div>
+        <h1>Dark Weather</h1>
+      </div>
+      <div>
+        <p>Home</p>
+        <p>Download App</p>
+        <p>Contact us</p>
+      </div>
+      <div>
+        <button>Sign up</button>
+      </div>
+    </nav>
+  );
+}
+
+export {Navbar};
---
+++ Weather/src/Page/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface HomeProps{
+}
+
+const Home: React.FC <HomeProps> =(props)=>{
+  return(
+    <>
+      
+    </>
+  );
+};
+export {Home};
